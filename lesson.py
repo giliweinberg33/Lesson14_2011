9068b71e5dd0c677758454ad3581b913a44a8f6f
Gili Weinber <gmmg33@gmail.com>
2024-11-20 19:13:05 +0200

    first commit

---
+++ lesson.py
@@ -0,0 +1,14 @@
+# non functional test - srtess testing,
+
+# GIT
+# backup tool
+# deel conflicts - version control, we can do roll control
+# Repository:
+# folder - .git -
+# commit - time & date, info (user and coment) (in .git)
+# in cmd: mkdir - make folder, cd - go to that diraction, git init
+# bash - we can get there in pycharm terminal
+    # git config --global user.name "Gili Weinber"
+    # git config --global user.email "gmmg33@gmail.com"
+    # git init
+    # git status
